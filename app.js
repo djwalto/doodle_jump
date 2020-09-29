0b896809a7af10136bf4f7f5bf8a158d063e9ce9
David Walton <davidwalton6772@gmail.com>
2020-09-29 15:38:50 -0500

    left and right and jump functions created

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
     const grid = document.querySelector('.grid')
     const doodler = document.createElement('div')
     let doodlerLeftSpace = 50
-    let doodlerBottomSpace = 150
+    let startPoint = 150
+    let doodlerBottomSpace = startPoint
     let isGameOver = false
     let platformCount = 5
+    let platforms = []
+    let upTimerId
+    let downTimerId
+    let isJumping = true
+    let isGoingLeft = false
+    let isGoingRight = false
+    let leftTimerId
+    let rightTimerId
+
 
     function createDoodler() {
         grid.appendChild(doodler)
         doodler.classList.add('doodler')
+        doodlerLeftSpace = platforms[0].left
         doodler.style.left = doodlerLeftSpace + 'px';
         doodler.style.bottom = doodlerBottomSpace + 'px';
     }
@@ -32,13 +43,111 @@ document.addEventListener('DOMContentLoaded', () => {
             let platformGap = 600 / platformCount
             let newPlatformBottom = 100 + i * platformGap
             let newPlatform = new Platform(newPlatformBottom)
+            platforms.push(newPlatform)
+            console.log(platforms)
         }
+    }
+
+    function movePlatforms() {
+        if (doodlerBottomSpace > 200) {
+            platforms.forEach(platform => {
+                platform.bottom -= 4
+                let visual = platform.visual
+                visual.style.bottom = platform.bottom + 'px'
+            })
+        }
+    }
+
+    function jump() {
+        clearInterval(downTimerId)
+        isJumping = true
+        upTimerId = setInterval(function () {
+            doodlerBottomSpace += 20
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace > startPoint + 200) {
+                fall()
+            }
+        }, 30)
+    }
+
+    function fall() {
+        clearInterval(upTimerId)
+        isJumping = false
+        downTimerId = setInterval(function () {
+            doodlerBottomSpace -= 5
+            doodler.style.bottom = doodlerBottomSpace + 'px'
+            if (doodlerBottomSpace <= 0) {
+                gameOver()
+
+            }
+            platforms.forEach(platform => {
+                if (
+                    (doodlerBottomSpace >= platform.bottom) &&
+                    (doodlerBottomSpace <= platform.bottom + 15) &&
+                    ((doodlerLeftSpace + 60) >= platform.left) &&
+                    (doodlerLeftSpace <= (platform.left + 85)) &&
+                    !isJumping
+
+                ) {
+                    console.log('landed')
+                    startPoint = doodlerBottomSpace
+                    jump()
+                }
+            })
+        }, 30)
+    }
+
+
+    function gameOver() {
+        console.log('game over')
+        isGameOver = true
+        clearInterval(upTimerId)
+        clearInterval(downTimerId)
+    }
+
+    function control(e) {
+        if (e.key === "ArrowLeft") {
+            moveLeft()
+
+        }
+        //
+        else if (e.key === "ArrowRight") {
+
+        }
+        //
+        else if (e.key === "ArrowUp") {
+        }
+    }
+
+    function moveLeft() {
+        isGoingLeft = true
+        leftTimerId = setInterval(function () {
+            if (doodlerLeftSpace >= 0) {
+                doodlerLeftSpace -= 5
+                doodler.style.left = doodlerLeftSpace + 'px'
+            }
+        }, 30)
+    }
+
+    function moveRight() {
+        isGoingRight = true
+        rightTimerId = setInterval(function () {
+            if (
+                doodlerLeftSpace <= 340) {
+                doodleLeftSpace += 50
+                doodler.style.left = doodlerLeftSpace + 'px'
+            } else moveLeft()
+        }, 30)
     }
 
     function start() {
         if (!isGameOver) {
-            createDoodler()
+
             createPlatforms()
+            createDoodler()
+            setInterval(movePlatforms, 30)
+            jump()
+            document.addEventListener('keyup', control)
         }
     }
     // make button
